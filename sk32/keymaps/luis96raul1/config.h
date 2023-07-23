9390ea80c5c822dd40812107b0fa06a0bb5e310f
Luis <luis96raul1@gmail.com>
2023-07-23 16:05:06 -0500

    Trying to recover

--- sk32/keymaps/luis96raul1/config.h
+++ sk32/keymaps/luis96raul1/config.h
@@ -6,16 +6,16 @@
 
 #define VIAL_KEYBOARD_UID {0x63, 0x8B, 0x74, 0x2F, 0xE2, 0x8C, 0xAB, 0xC1}
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 8  // LAYERS FOR KEYMAPS
+#define DYNAMIC_KEYMAP_LAYER_COUNT 9  // LAYERS FOR KEYMAPS
 
 // TAP DANCE
-#define TAPPING_TERM 300
+#define TAPPING_TERM 180
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #define VIALRGB_NO_DIRECT
 
 // DEFINE MASTER
-#define MASTER_LEFT
+#define MASTER_RIGHT
 
 // OLED FONT
 #ifdef OLED_ENABLE
@@ -38,8 +38,8 @@
 #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5  // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16                            // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150                        // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-#define RGB_MATRIX_HUE_STEP 3
-#define RGB_MATRIX_SAT_STEP 8
+#define RGB_MATRIX_HUE_STEP 1
+#define RGB_MATRIX_SAT_STEP 1
 #define RGB_MATRIX_VAL_STEP 8
 #define RGB_MATRIX_SPD_STEP 10
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
